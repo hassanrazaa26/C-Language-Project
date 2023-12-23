49fce35e8b795751fb5a480453bf58f199dc9ddd
Mohammad Hassan Raza <149901198+hassanrazaa26@users.noreply.github.com>
2023-12-23 11:57:06 +0500

    Add files via upload

---
+++ Math Table.cpp
@@ -0,0 +1,48 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	int tab,len,c,d;
+	
+	printf(" \n");
+	printf("                ' W  E  L     C  O  M  E '\n");
+	printf("                ``````````````````````````\n");
+
+	printf("---------------------------------------------------------\n");
+	printf(" Table of Mathamatic by:    MOHAMMAD  HASSAN  RAZA \n");
+	printf("---------------------------------------------------------\n");
+	printf(" \n");
+	printf(" \n");
+	printf(" \n");
+	printf("---------------------------------------------------------\n");
+	printf("  Enter Number for Table: ");
+	scanf("%d",&tab);
+	printf("---------------------------------------------------------\n");
+	
+	printf(" \n");
+	printf("---------------------------------------------------------\n");
+	printf("  Enter Length  of Table: ");
+	scanf("%d",&len);
+	printf("---------------------------------------------------------\n");
+
+	printf(" \n");
+	printf(" \n");
+	printf("                  -.~.~.~.~.~.~.~.-                                 \n");
+	for(c=1;c<=len;c++)
+	printf("                     %d x %d = %d \n",tab,c,tab*c);
+	printf(" \n");
+	printf("                  -.~.~.~.~.~.~.~.-                                 \n");
+	
+	printf(" \n");
+    printf(" \n");
+    printf("------------------------------------------------------------\n");
+    printf("' M  O  H  A  M  M  A  D    H  A  S  S  A  N    R  A  Z  A '\n");
+    printf("------------------------------------------------------------\n");
+    
+printf("  \n ");
+printf("   \n");
+printf(" -------------------------\n");
+printf(" Thanks   For  Visit...\n");
+printf(" -------------------------\n");
+	getch();
+}
